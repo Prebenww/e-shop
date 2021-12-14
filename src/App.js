93272f344d00f28bba0fd07a31516f5c4d84ae44
Prebenww <63924500+Prebenww@users.noreply.github.com>
2021-12-14 14:05:45 +0100

    Oppdaterer appen med shop page, collection preview component og collection item component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import HomePage from "./components/pages/homepage.component";
+import HomePage from "./components/pages/homepage/homepage.component";
+import {Switch, Route} from "react-router-dom";
+import ShopPage from "./components/pages/shop/shop.component";
+
 
 function App() {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/shop" component={ShopPage}/>
+
+            </Switch>
+        </div>
+    );
 }
+
 
 export default App;
--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import SHOP_DATA from "./shop.data";
+
+import {CollectionPreview} from "../../collection-preview/collection-preview";
 
 class ShopPage extends React.Component {
     constructor(props) {
-       super(props);
+        super(props);
 
-       this.state = {
-           collection: 
-       }
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div className='shop-page'>
+                {collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))}
+            </div>
+        );
     }
 }
 
