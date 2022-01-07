4302b5f0f398ad26fac149d334e9eecfe882fda0
Prebenww <63924500+Prebenww@users.noreply.github.com>
2022-01-07 14:25:49 +0100

    lagt til cart-icon og cart-dropdown, implementert redux i cart.

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 import "./collection-preview.component.scss";
 
-import {CollectionItem} from "../collection-item/collection-item.component";
+import CollectionItem from "../collection-item/collection-item.component";
 
 export const CollectionPreview = ({title, items}) => (
-    <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <div className="preview">
-            {
-                items
-                    .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem key={id}{...otherItemProps} />
-                    ))
-            }
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
+                ))}
         </div>
     </div>
 )
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,12 +5,13 @@ import {connect} from "react-redux";
 import "./header.styles.scss"
 import {Link} from "react-router-dom";
 
-
 import {ReactComponent as Logo} from "../../assets/crown.svg";
 import {auth} from "../../firebase/firebase.utils";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
 
-const Header = ({ currentUser }) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -24,16 +25,22 @@ const Header = ({ currentUser }) => (
             </Link>
             {
                 currentUser ?
-                    <div className="option" onClick={() => auth.signOut()} >SIGN OUT </div>
+                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT </div>
                     :
                     <Link className="option">SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
+
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden,
 });
 
 export default connect(mapStateToProps)(Header);
